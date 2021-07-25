3ff7bddc52f469f8146faa5535e10b4ce0e69927
Alex <alexmatvichuk@gmail.com>
2021-07-25 15:57:17 +0200

    make fatch function async

--- src/js/fetch-images.js
+++ src/js/fetch-images.js
@@ -1,14 +1,12 @@
 import axios from 'axios';
 
-export default function fetchImages(searchEl) {
+export default async function fetchImages(searchEl) {
   const BASE_URL = 'https://pixabay.com/api/';
   const PIXABAY_KEY = '22634549-5cdc48e9fdfcb009c2ce01724';
 
-  return axios
-    .get(
-      `${BASE_URL}?key=${PIXABAY_KEY}&q=${searchEl}&image_type=photo&orientation=horizontal&safesearch=true`,
-    )
-    .then(response => {
-      return response.data.hits;
-    });
+  const response = await axios.get(
+    `${BASE_URL}?key=${PIXABAY_KEY}&q=${searchEl}&image_type=photo&orientation=horizontal&safesearch=true`,
+  );
+
+  return response.data.hits;
 }
